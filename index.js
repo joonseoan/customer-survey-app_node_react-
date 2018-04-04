06d41686d520be3f64222af5175db228c69994ca
Joon An <joonseo.an@outlook.com>
2018-04-04 17:57:31 -0400

    finished permission request to google

--- index.js
+++ index.js
@@ -1,28 +1,81 @@
 console.log('starting index.js of "server" part');
 
-// It is a common js module.
 const express = require('express');
 
-// ES 15 module
-//import express from 'express';
+const passport = require('passport');
 
-// Call "express()" returns the "running express app"
-//       assigned to "app" variable.
-// The reason it is not a component of express,
-//      express returns a single app, "running express app" 
-// the running express app managing and control the input from the browser,
-//      then hands off the input to the routHandler.
+// "Strategy" is a component of "passport-google-oauth20"
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-// Official Definition : Express App to register this
-//      route handler with
+const { googleClientID, googleClientSecret } = require('./config/keys');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
+// "use" is a method to access GoogleStrategy 
+// Then, by using "()" of the object, it makes Google configure 
+//      the authentication process for the application.
 
-    res.send({ bye : 'Sheridan' });
+/*
+// In order for Google to correctly configure the auth process,
+//      we need to give Google information about the application.
+// They are "client id" and "client secret"
 
-});
+// For instance, we need to inform Google 
+//      where the users are from for the auth.
+// Then, Google will send them back some (API) info for the auth process.
+// To establish this process, we need to sign up with Google OAuth.
+*/
 
-const PORT  = process.env.PORT || 5000;
+// Once we finish clientID and client secret, 
+//      we need to inform Google both.
+passport.use(
+    
+    new GoogleStrategy({
+
+        // "clientID" and "clientSecret" are built-in name.
+        clientID : googleClientID,
+        clientSecret : googleClientSecret,
+
+        // It assigns the route / directory
+        //      where / which route Google should ping the permission
+        //      at this app server, after it listens to and investigates
+        //      the request form the user and then this app server
+        callbackURL : '/auth/google/callback'
+
+    }, (accesToken) => {
+
+        // later on.
+        console.log(accessToken);
+
+    })
+
+);
+
+// Route Handler to listen to the user access to the define route "/auth/google".
+//      => It is a kind of the first user request.
+// Then it will try to send the info including "clientID" and "clientSecret"
+//      for the configuration.
+
+// We will not use callback because we are using "passport" MW.
+// The " passport~ " below is to process "send" authenticate response info to
+//      google server, not back to the user.
+
+// 1) 'google', the first argument defines that the strategy defined above
+//      for the google strategis other than any other app.
+// Therefore 'google' stands for containing the information about
+//      GoogleStrategy object. 
+
+// 2) Google defines different properties for the auth.
+//    'scope' defines the properties we want? to use for auth
+//      ,among many different properties.
+// In this app, we will use "profile" and "email" only.
+app.get('/auth/google', passport.authenticate('google', {
+
+    scope : ['profile', 'email']
+
+}));
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
