3e53a1ef3da16bc3f384a110f2f44b01cda4e06e
Joon An <joonseo.an@outlook.com>
2018-04-16 16:58:36 -0400

    changed keys

--- config/keys.js
+++ config/keys.js
@@ -4,12 +4,10 @@ if(process.env.NODE_ENV === 'production') {
 
     // We are in product. Return the product keys here in this file.
     module.exports = require('./prod');
-    console.log(require('./prod'));
 
 } else {
 
     //We are in development - return the devkey.
     module.exports = require('./dev');
-    console.log(require('./dev'));
 
 }
