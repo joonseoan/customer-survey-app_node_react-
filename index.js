5a278caeeed4634b5437c4800d082479b3cff20f
Joon An <joonseo.an@outlook.com>
2018-05-31 01:44:50 -0400

    fintal test

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-console.log(`starting on ${PORT}`);
+//console.log(`starting on ${PORT}`);
 app.listen(PORT);
 
 // Just for studying....
